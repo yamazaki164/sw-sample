9061b1d65ca35fc0952b89859680dbde631b9ccc
yamazaki164 <yamazaki164@gmail.com>
2020-12-12 13:55:46 +0900

    update

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js');
-};
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./sw.js')
+    .then((reg) => console.log('sw registerd'))
+    .catch((err) => console.log('error ', err));
+
+  setTimeout(() => {
+    const img = new Image();
+
+    img.src = '/dog.svg';
+    document.body.appendChild(img);
+  }, 3000);
+}
--- sw.js
+++ sw.js
@@ -1,11 +1,19 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Install');
+
+  event.waitUntil(
+    caches.open('static-v1').then((cache) => cache.add('/cat.svg'))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    console.log('[Service Worker] Fetched resource ' + e.request.url);
+self.addEventListener('fetch', (event) => {
+  console.log('[Service Worker] Fetched resource ' + event.request.url);
+
+  if (url.origin == location.origin && url.pathname == '/dog.svg') {
+    event.respondWith(caches.match('/cat.svg'));
+  }
 });
 
 self.addEventListener('activate', (e) => {
-    console.log('[Service Worker] Activate');
+  console.log('[Service Worker] Activate');
 });
