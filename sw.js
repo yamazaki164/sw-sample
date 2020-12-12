0bc758498fa46d2ece7dfb1dddbdba912a905bd3
yamazaki164 <yamazaki164@gmail.com>
2020-12-12 12:26:40 +0900

    update

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
 });
+
+self.addEventListener('fetch', (e) => {
+    console.log('[Service Worker] Fetched resource ' + e.request.url);
+});
+
+self.addEventListener('activate', (e) => {
+    console.log('[Service Worker] Activate');
+});
