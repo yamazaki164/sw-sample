821582d8c5aac0f3f8e42d52188752cf38340b32
yamazaki164 <yamazaki164@gmail.com>
2020-12-13 21:32:13 +0900

    update

--- sw.js
+++ sw.js
@@ -1,19 +1,24 @@
 self.addEventListener('install', (event) => {
+  document.querySelector('#state').innerHTML('[Service Worker] Install');
   console.log('[Service Worker] Install');
 
   event.waitUntil(
-    caches.open('static-v1').then((cache) => cache.add('/sw-sample/cat.svg'))
+    caches.open('static-v1').then((cache) => cache.add('/sw-sample/cat.png'))
   );
 });
 
 self.addEventListener('fetch', (event) => {
+  document
+    .querySelector('#state')
+    .innerHTML('[Service Worker] Fetched resource ' + event.request.url);
   console.log('[Service Worker] Fetched resource ' + event.request.url);
 
-  if (url.origin == location.origin && url.pathname == '/sw-sample/dog.svg') {
-    event.respondWith(caches.match('/sw-sample/cat.svg'));
+  if (url.origin == location.origin && url.pathname == '/sw-sample/dog.png') {
+    event.respondWith(caches.match('/sw-sample/cat.png'));
   }
 });
 
-self.addEventListener('activate', (e) => {
+self.addEventListener('activate', (event) => {
+  document.querySelector('#state').innerHTML('[Service Worker] Activate');
   console.log('[Service Worker] Activate');
 });
