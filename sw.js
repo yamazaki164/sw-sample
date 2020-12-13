862e18c578ed2b97b23c5478bef118a114d0e2b2
yamazaki164 <yamazaki164@gmail.com>
2020-12-13 21:08:50 +0900

    update

--- sw.js
+++ sw.js
@@ -2,15 +2,15 @@ self.addEventListener('install', (event) => {
   console.log('[Service Worker] Install');
 
   event.waitUntil(
-    caches.open('static-v1').then((cache) => cache.add('/cat.svg'))
+    caches.open('static-v1').then((cache) => cache.add('/sw-sample/cat.svg'))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   console.log('[Service Worker] Fetched resource ' + event.request.url);
 
-  if (url.origin == location.origin && url.pathname == '/dog.svg') {
-    event.respondWith(caches.match('/cat.svg'));
+  if (url.origin == location.origin && url.pathname == '/sw-sample/dog.svg') {
+    event.respondWith(caches.match('/sw-sample/cat.svg'));
   }
 });
 
